ed6ed105939ff92130668fe3b74a493afb9de52a
lalla <juanlalla@gmail.com>
2017-03-01 12:57:07 -0300

    Agrego la clase que representa a los artículos del Blog

---
+++ Blog/Models/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    /// <summary>
+    /// Entidad que representa a los artículos del Blog
+    /// </summary>
+    public class Articulo
+    {
+        public long ID { get; set; }
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+        //autor... comentarios...
+    }
+}
